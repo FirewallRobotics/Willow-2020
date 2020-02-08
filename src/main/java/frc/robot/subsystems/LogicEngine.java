bc5ec2e92d5fb6e134358dbf988164831cd3bea5
Tyler Cartier <tylercartier05@gmail.com>
2020-02-08 10:40:58 -0500

    Motor Controllers Put In

--- src/main/java/frc/robot/subsystems/LogicEngine.java
+++ src/main/java/frc/robot/subsystems/LogicEngine.java
@@ -62,6 +62,9 @@ public class LogicEngine extends Subsystem {
     public void periodic() {
         // Put code here to be run every loop
             XboxController controller = Robot.oi.getXboxController();
+            // Willow
+            //DriveTrain.Drive(-controller.getY(GenericHID.Hand.kLeft),controller.getX(GenericHID.Hand.kRight),controller.getBumperPressed(GenericHID.Hand.kRight));
+            // MiniWillow
             DriveTrain.Drive(controller.getY(GenericHID.Hand.kLeft),-controller.getX(GenericHID.Hand.kRight),controller.getBumperPressed(GenericHID.Hand.kRight));
         
     }

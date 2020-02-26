382c40adc393441f859b2574f3f77332c4466c1a
Tyler Cartier <tylercartier05@gmail.com>
2020-02-25 19:56:44 -0500

    j

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -133,11 +133,10 @@ public class Shooter extends Subsystem {
     public static void shoot(double triggerAxis) {
             if (triggerAxis < RobotMap.LOW_JOY_SPEED) {
                 Shooter.set(0);
-                Shooter.set(ControlMode.Velocity, 23250);
                 //Shooter2.set(0);
             }
             else if (triggerAxis >= RobotMap.LOW_JOY_SPEED && triggerAxis <= RobotMap.FULL_JOY_SPEED) {
-                Shooter.set(RobotMap.LOW_SHOOTER_SPEED);
+                Shooter.set(ControlMode.Velocity, 23250);
                 //Shooter2.set(RobotMap.LOW_SHOOTER_SPEED);
             }
             else {
